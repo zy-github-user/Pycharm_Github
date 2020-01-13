4849fe28ed05e8571beffad844fc778041b9c979
赵琰 <zhaoyan@zhaoyandeMacBook-Pro.local>
2020-01-13 10:29:28 +0800

    this is working projects

---
+++ untitled/test.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import addressbook_pb2
+import sys
+
+person = addressbook_pb2.Person()
+person.id = 1234
+person.name = "John Doe"
+person.email = "jdoe@example.com"
+phone = person.phones.add()
+phone.number = "555-4321"
+phone.type = addressbook_pb2.Person.HOME
+
+personString = person.SerializeToString()
+# phoneString = phone.SerializeToString()
+
+oo = addressbook_pb2.Person()
+oo.ParseFromString(personString)
+
+print(oo.id)
+print(oo.name)
+print(oo.email)
